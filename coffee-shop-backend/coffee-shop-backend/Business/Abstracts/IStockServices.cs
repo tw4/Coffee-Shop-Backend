e48e587f2b3daaab3be19a88b0ff7cf4d65a54f1
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-11-30 09:40:21 +0300

    update(backend): docker-compose

---
+++ coffee-shop-backend/coffee-shop-backend/Business/Abstracts/IStockServices.cs
@@ -0,0 +1,6 @@
+namespace coffee_shop_backend.Business.Abstracts;
+
+public interface IStockServices
+{
+    
+}
---
+++ coffee-shop-backend/coffee-shop-backend/Controllers/StockController.cs
@@ -0,0 +1,6 @@
+namespace coffee_shop_backend.Controllers;
+
+public class StockController
+{
+    
+}
