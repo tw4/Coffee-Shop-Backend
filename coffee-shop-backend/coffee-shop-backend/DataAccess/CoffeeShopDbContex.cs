01a436dbd3b22e08fbf81344b6e96f7a689c5d6a
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-12-01 14:05:30 +0300

    feat(backend): seed admin user

--- coffee-shop-backend/coffee-shop-backend/DataAccess/CoffeeShopDbContex.cs
+++ coffee-shop-backend/coffee-shop-backend/DataAccess/CoffeeShopDbContex.cs
@@ -7,10 +7,10 @@ public class CoffeeShopDbContex : DbContext
 {
     private readonly IConfiguration _configuration;
 
-   public DbSet<User> Users { get; set; }
-   public DbSet<Product> Products { get; set; }
-   public DbSet<Stock> Stocks { get; set; }
-   public DbSet<Order> Orders { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Stock> Stocks { get; set; }
+    public DbSet<Order> Orders { get; set; }
 
     public CoffeeShopDbContex(DbContextOptions<CoffeeShopDbContex> options) : base(options)
     {
@@ -20,7 +20,24 @@ public class CoffeeShopDbContex : DbContext
     {
         if (!optionsBuilder.IsConfigured)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=mydatabase;Username=myuser;Password=mypassword");
+            optionsBuilder.UseNpgsql(
+                "Host=localhost;Port=5432;Database=mydatabase;Username=myuser;Password=mypassword");
         }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // seed admin user
+        modelBuilder.Entity<User>().HasData(
+            new User
+            {
+                Id = 1,
+                Name = "Admin",
+                Surname = "Admin",
+                Email = "admin@example.com",
+                Password = "123456",
+                Role = EnumRole.ADMIN
+            }
+        );
     }
 }
