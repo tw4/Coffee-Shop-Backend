ff74a6711a21add3c7891188f68553c7a2d64495
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-12-20 15:41:35 +0300

    feat(backend): get user email from token

---
+++ coffee-shop-backend/coffee-shop-backend.Tests/CoffeeShopTestDbContex.cs
@@ -0,0 +1,6 @@
+namespace coffee_shop_backend.Tests;
+
+public class CoffeeShopTestDbContex
+{
+    
+}
--- coffee-shop-backend/coffee-shop-backend/Business/Abstracts/IJwtServices.cs
+++ coffee-shop-backend/coffee-shop-backend/Business/Abstracts/IJwtServices.cs
@@ -5,4 +5,5 @@ public interface IJwtServices
    public string GenerateJwtToken(long Id, string email);
    public long GetUserIdFromToken(string token);
    public bool IsTokenValid(string token);
+   public string GetUserEmailFromToken(string token);
 }
