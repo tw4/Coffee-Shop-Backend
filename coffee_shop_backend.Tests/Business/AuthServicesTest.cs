7a7e0fefc1a6a809f0e9e0dee4a603a22278263b
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-12-21 21:09:04 +0300

    delete folder

--- coffee_shop_backend.Tests/Business/AuthServicesTest.cs
+++
@@ -1,6 +0,0 @@
-namespace coffee_shop_backend.Tests.Business;
-
-public class AuthServicesTest
-{
-    
-}
--- coffee_shop_backend.Tests/Controller/AuthControllerTest.cs
+++
@@ -1,6 +0,0 @@
-namespace coffee_shop_backend.Tests.Controller;
-
-public class AuthControllerTest
-{
-    
-}
