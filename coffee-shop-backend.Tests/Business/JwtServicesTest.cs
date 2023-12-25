bd87433bfe61ea518342b723ff58b46a835dd972
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-12-25 15:09:31 +0300

    refactor(backend.test) jwt services test

--- coffee-shop-backend.Tests/Business/JwtServicesTest.cs
+++ coffee-shop-backend.Tests/Business/JwtServicesTest.cs
@@ -4,24 +4,21 @@ using Microsoft.Extensions.Logging;
 
 namespace coffee_shop_backend.Tests.Business;
 
-public class JwtServicesTest
+public class JwtServicesTest: IClassFixture<JwtServicesFixture>
 {
-    private readonly IConfiguration _configuration;
-    private readonly Logger<JwtServices> _logger;
 
-    public JwtServicesTest()
+    private JwtServicesFixture _fixture;
+    public JwtServicesTest( JwtServicesFixture fixture)
     {
-        _configuration = TestHelper.CreateConfiguration();
-        _logger = new Logger<JwtServices>(new LoggerFactory());
+        _fixture = fixture;
     }
 
     [Fact]
     public void JwtServices_GenerateJwtToken_Returns_Token()
     {
-        var jwtServices = new JwtServices(_configuration, _logger);
-        var id = 1;
-        var email = "test_email";
-        var token = jwtServices.GenerateJwtToken(id, email);
+        var jwtServices = _fixture.CreateJwtServices();
+        var user = TestHelper.GetTestUser();
+        var token = jwtServices.GenerateJwtToken(user.Id, user.Email);
 
         Assert.NotNull(token);
         Assert.IsType<string>(token);
@@ -30,37 +27,34 @@ public class JwtServicesTest
     [Fact]
     public void JwtServices_GenerateJwtToken_Returns_UserId()
     {
-        var jwtServices = new JwtServices(_configuration, _logger);
-        var id = 1;
-        var email = "test_email";
-        var token = jwtServices.GenerateJwtToken(id, email);
+        var jwtServices = _fixture.CreateJwtServices();
+        var user = TestHelper.GetTestUser();
+        var token = jwtServices.GenerateJwtToken(user.Id, user.Email);
         var userId = jwtServices.GetUserIdFromToken(token);
 
         Assert.IsType<long>(userId);
-        Assert.Equal(id, userId);
+        Assert.Equal(user.Id, userId);
     }
 
     [Fact]
     public void JwtServices_GenereateJwtToken_Returns_UserEmail()
     {
-        var jwtServices = new JwtServices(_configuration, _logger);
-        var id = 1;
-        var email = "test_email";
-        var token = jwtServices.GenerateJwtToken(id, email);
+        var jwtServices = _fixture.CreateJwtServices();
+        var user = TestHelper.GetTestUser();
+        var token = jwtServices.GenerateJwtToken(user.Id, user.Email);
         var userEmail = jwtServices.GetUserEmailFromToken(token);
 
         Assert.NotNull(userEmail);
         Assert.IsType<string>(userEmail);
-        Assert.Equal(email, userEmail);
+        Assert.Equal(user.Email, userEmail);
     }
 
     [Fact]
     public void JwtServices_ValidateToken()
     {
-        var jwtServices = new JwtServices(_configuration, _logger);
-        var id = 1;
-        var email = "test_email";
-        var token = jwtServices.GenerateJwtToken(id, email);
+        var jwtServices = _fixture.CreateJwtServices();
+        var user = TestHelper.GetTestUser();
+        var token = jwtServices.GenerateJwtToken(user.Id, user.Email);
         var isValid = jwtServices.IsTokenValid(token);
 
         Assert.True(isValid);
